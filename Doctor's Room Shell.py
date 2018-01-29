655ef5dd9e910961a49993717ee4621c3f2dd47c
zackbell123 <35306158+zackbell123@users.noreply.github.com>
2018-01-28 22:18:13 -0600

    Add files via upload

---
+++ Doctor's Room Shell.py
@@ -0,0 +1,14 @@
+def main():
+    class Doctor():
+        def __init__(self):
+    class Nurse():
+        def __init__(self):
+    class Patient():
+        def __init__(self):
+    class wroom():
+        def __init__(self, pnum):
+            self.pnum = pnum
+    class eroom():
+        def __init__(self, etime):
+            self.etime = etime
+            
